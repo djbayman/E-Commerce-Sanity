2ce9c4f089a870f0652af0d580aaf724889ca02c
djbayman <djaballahayman20@gmail.com>
2024-11-05 11:05:39 +0100

    CORS vercel url

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://buyliecomm.vercel.app" }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "web/build")));
